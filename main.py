f432d5307893938827a26692f731eedc5f2b7746
viraj812 <124186730+viraj812@users.noreply.github.com>
2023-10-29 17:17:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,28 @@
 from bert_processing import bert_predict_
 from text_to_speech import speak_, speech_to_text_
 from PyPDF2 import PdfReader
+from gensim.summarization import summarize
 
 def getContext():
-    reader = PdfReader("book.pdf")
+    reader = PdfReader("../static/progit.pdf")
     # page = reader.pages[0]
     page = [i.extract_text() for i in reader.pages]
-    page = [i.replace("\n", "") for i in page]
+    page = [i.replace("\n", " ") for i in page]
     text = ""
 
     for line in page:
         text += line
     # print(text)
+
+    text = summarize(text, ratio=0.25)
     return text
 
 def main():
-    book = open("book.txt", 'r', encoding='utf-8').readlines()
-    text = ""
-
-    for line in book:
-        text += line
-    # question = speech_to_text_()
-    question = "what is an algorithm?"
+    text = getContext()
+    # print(text)
+    question = "what is version control?"
     output = bert_predict_(question, text)
     speak_(output)
-
-
-# getContext("hello")
 
 if __name__ == '__main__':
     main()
